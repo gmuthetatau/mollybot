b5b66ed93592f918d5a61fc9ff21bbce2c5f21c0
roy4le <nolascoal.eli@hotmail.com>
2016-12-03 19:27:00 -0500

    Word Shuffler

---
+++ al.py
@@ -0,0 +1,43 @@
+from reddit import reddit
+from task import Task
+from reddit import getFrontPage
+from random import shuffle
+
+class Al(Task):
+
+	def shuffle_word(self, word):
+		word = list(word)
+		shuffle(word)
+		return ''.join(word)
+
+	def dislexia(self, list):
+		final = []
+		for item in list:
+			text = item.title
+			tlist = text.split(" ")
+			c = 0
+			l = ""
+			while c < len(tlist):
+				word = tlist[c]
+				if len(word) > 3:
+					x = word[0]
+					y = word[-1]
+					wshuffle = word[1 : -1]
+					shuffled =self.shuffle_word(wshuffle)
+					fword = ("" + x + shuffled + y)
+					#print word
+					#print wshuffle
+					#print fword
+					l += fword + " "
+				else:
+					l += word + " "
+				c += 1
+			return l
+
+
+	def run(self):
+		self.body = self.dislexia(getFrontPage())
+		self.title = "Chris test task"
+
+
+al = Al()
